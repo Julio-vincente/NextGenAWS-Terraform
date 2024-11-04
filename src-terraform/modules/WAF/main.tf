5cc30904fabcf232cf6517fe1f1d423be135be4e
julio <juliomacedo.vicente@gmail.com>
2024-11-04 12:29:50 -0300

    add: module waf, and apply

---
+++ src-terraform/modules/WAF/main.tf
@@ -0,0 +1,57 @@
+resource "aws_wafv2_web_acl" "waf_prod" {
+  name        = var.waf_prod_name
+  description = var.waf_prod_description
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = var.waf_metric_name
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = var.waf_prod_rule_name
+    priority = 1
+    statement {
+      managed_rule_group_statement {
+        name        = var.waf_prod_rule_name
+        vendor_name = var.vendor_name
+      }
+    }
+
+    override_action {
+      none {}
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = var.waf_prod_rule_name_cors
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = var.waf_sql_rule
+    priority = 2
+
+    statement {
+      managed_rule_group_statement {
+        name        = var.waf_sql_name_rule
+        vendor_name = var.vendor_name
+      }
+    }
+
+    override_action {
+      none {}
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = var.waf_sql_rule
+      sampled_requests_enabled   = true
+    }
+  }
+}
---
+++ src-terraform/modules/WAF/variables.tf
@@ -0,0 +1,46 @@
+variable "waf_prod_name" {
+  description = "Nome do WAF Web ACL"
+  type        = string
+  default = "my-waf-prod"
+}
+
+variable "waf_prod_description" {
+  description = "Descrição do WAF Web ACL"
+  type        = string
+  default = "WAF Web ACL for production"
+}
+
+variable "waf_metric_name" {
+  description = "Nome do métrico no CloudWatch para o WAF"
+  type        = string
+  default = "my-waf-metrics"
+}
+
+variable "waf_prod_rule_name" {
+  description = "Nome da regra para Core Rule Set"
+  type        = string
+  default = "AWSManagedRulesCommonRuleSet"
+}
+
+variable "waf_prod_rule_name_cors" {
+  type    = string
+  default = "aws_managed_rules_crs"
+}
+
+variable "waf_sql_rule" {
+  description = "Nome da regra de SQL Injection"
+  type        = string
+  default = "SQLInjectionRule"
+}
+
+variable "waf_sql_name_rule" {
+  description = "Nome do conjunto de regras gerenciadas para SQL"
+  type        = string
+  default = "AWSManagedRulesSQLiRuleSet"
+}
+
+variable "vendor_name" {
+  description = "Nome do fornecedor da regra gerenciada (ex: AWS, OWASP)"
+  type        = string
+  default     = "AWS"
+}
